92e1e4227f758d654513b3f7040c39bdef345ef1
proteus-mx <paul.thornton@gmail.com>
2025-06-04 16:28:14 +1000

    Initial Commit

---
+++ Falls Creek Watch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Falls Creek Watch
+//
+//  Created by Paul Thornton on 4/6/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Falls Creek Watch/Falls_Creek_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Falls_Creek_WatchApp.swift
+//  Falls Creek Watch
+//
+//  Created by Paul Thornton on 4/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct Falls_Creek_WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
